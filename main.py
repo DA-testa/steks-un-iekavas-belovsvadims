2a6a4d0eec4fa41e18faadde6b6776ac6892853d
Vadim <58696690+belovsvadims@users.noreply.github.com>
2023-02-19 14:34:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ def find_mismatch(text):
 
 def main():
     text = input()
+    if input == "I":
+        text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
     print(mismatch)
